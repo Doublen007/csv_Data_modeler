c87b2f4c008609423c9903769937f6bb69dd6ace
Doublen007 <126976869+Doublen007@users.noreply.github.com>
2023-07-31 15:43:11 -0500

    Add files via upload

---
+++ DataModeler 2D.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def modeler2D(filename, x_column, y_column):
+    # Read file
+    df = pd.read_excel(filename)
+
+    # convert column data to lists
+    xRaw = df[x_column]
+    x = list(xRaw)
+    yRaw = df[y_column]
+    y = list(yRaw)
+
+    # Plotting
+    plt.scatter(x, y, color='blue', marker='*', s=15)
+
+    # ***BOUNDS***
+    # plt.xlim([.8e10, 1.2e10])
+    # plt.ylim([-1, 4])
+
+    plt.xlabel(x_column)
+    plt.ylabel(y_column)
+    plt.title(f' {y_column} vs. {x_column}')
+
+    # ***INVERT AXES***
+    # plt.gca().invert_xaxis()
+    # plt.gca().invert_yaxis()
+
+    plt.show()
+
+
